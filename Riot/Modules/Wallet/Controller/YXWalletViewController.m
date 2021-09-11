4f9ebf7a9f36685a9f96bb6e233d70ab05d130af
廖燊 <liaoshen@mama.cn>
2021-09-11 16:47:46 +0800

    no message

--- Riot/Modules/Wallet/Controller/YXWalletViewController.m
+++ Riot/Modules/Wallet/Controller/YXWalletViewController.m
@@ -251,8 +251,7 @@
             //检验成功，同事再次更新页面数据
             
             if (weakSelf.isCheckPassword) {
-                
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(<#delayInSeconds#> * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                     [weakSelf.viewModel.sectionItems removeAllObjects];
                     [weakSelf refreshHeaderAction];
                     [weakSelf.viewModel getAllCoinInfo];
